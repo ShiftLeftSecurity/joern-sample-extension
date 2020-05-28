a76f531de69dd28c03d2197a57b96b2990955012
Fabian Yamaguchi <fabs@shiftleft.io>
2020-05-28 22:16:40 +0200

    scalafmt, a bit of work on README.md.

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 Joern Git Extension
 ===================
 
+A sample extension for Joern/Ocular that determines recently edited
+files from a git repository and tags the corresponding file nodes in
+the Code Property Graph. Its main purpose it to show how Joern can be
+used as a platform for code analysis.
+
+Intro
+-----
+
 Joern/Ocular provide a runtime extension mechanism that allows you to
 develop custom analyzers on top of the platform. Extensions are
 programs for the JVM that can be written in Java, Scala or
-Kotlin. They can access the code property graph via the query language
-OcularQL and can create new nodes, edges and properties via the
+Kotlin. They can access the code property graph via a query language
+and create new nodes, edges and properties via the
 DiffGraph API. Moreover, they can interface with Java libraries to, for
 example, to access the network or file system.
 
@@ -35,11 +43,10 @@ start joern, type `run`. Your extension should be listed.
 
 ```bash
    cp target/universal/stage/lib/io.shiftleft.joern-sample-extension-*.jar $joern_install/lib/
-    cp target/universal/stage/lib/org.eclipse.jgit.org.eclipse.jgit* $joern_install/lib/
+   cp target/universal/stage/lib/org.eclipse.jgit.org.eclipse.jgit* $joern_install/lib/
 ```
 
 where `$joern_install` is the directory where you installed Joern/Ocular.
-
 
 Running
 -------
@@ -47,3 +54,18 @@ Running
 On the joern shell, type `run.gitextension`. You can also
 inspect/modify options passed to the plugin by typing
 `opts.gitextension.<TAB>`.
+
+
+Importing into IntelliJ and running tests
+-----------------------------------------
+
+Joern Extensions can be developed in an IDE and the process of
+importing an extension may differ slightly from IDE to IDE. The
+following instructions are for IntelliJ.
+
+![IntelliJ start screen](img/startscreen.png)
+
+
+![Just select the project](img/select.png)
+
+![Right click](img/runtests.png)
--- src/main/scala/io/shiftleft/gitextension/Gitextension.scala
+++ src/main/scala/io/shiftleft/gitextension/Gitextension.scala
@@ -1,12 +1,15 @@
 package io.shiftleft.gitextension
 
-import java.io.ByteArrayOutputStream
 import java.nio.file.{Path, Paths}
 
 import better.files._
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.passes.{CpgPass, DiffGraph}
-import io.shiftleft.semanticcpg.layers.{LayerCreator, LayerCreatorContext, LayerCreatorOptions}
+import io.shiftleft.semanticcpg.layers.{
+  LayerCreator,
+  LayerCreatorContext,
+  LayerCreatorOptions
+}
 import io.shiftleft.semanticcpg.language._
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.api.Git
@@ -19,84 +22,86 @@ import scala.jdk.CollectionConverters._
 object Gitextension {
 
   /**
-   * This is the extensions official name as will be shown in the table
-   * one obtains when running `run` on the Ocular shell.
-   * */
+    * This is the extensions official name as will be shown in the table
+    * one obtains when running `run` on the Ocular shell.
+    * */
   val overlayName = "Sample Git Extension"
 
   /**
-   * A short description to be shown in the table obtained when
-   * running `run` on the Ocular shell.
-   * */
+    * A short description to be shown in the table obtained when
+    * running `run` on the Ocular shell.
+    * */
   val description =
-    """
-      |A sample extension that tags all files that have recently been modified.
-      |The main purpose of this extension is to showcase how extensions can
-      |be written and which interfaces are available for them.
-      |""".stripMargin
+    "A sample extension that tags all files that have recently been modified."
 
   /**
-   * Option object initialize to defaults. This object will be made
-   * accessible to the user via `opts.myextension`.
-   * */
+    * Option object initialize to defaults. This object will be made
+    * accessible to the user via `opts.myextension`.
+    * */
   def defaultOpts = GitExtensionOpts(".")
 }
 
 /**
- * Options can be passed to the extension via a custom options
- * class that derives from `LayerCreatorOptions`. In our example,
- * we use the option class below to hand the path to the git
- * repository from the user to the extension.
- * */
-case class GitExtensionOpts(var pathToRepo : String) extends LayerCreatorOptions {}
+  * Options can be passed to the extension via a custom options
+  * class that derives from `LayerCreatorOptions`. In our example,
+  * we use the option class below to hand the path to the git
+  * repository from the user to the extension.
+  * */
+case class GitExtensionOpts(var pathToRepo: String)
+    extends LayerCreatorOptions {}
 
-class Gitextension(options : GitExtensionOpts) extends LayerCreator {
+class Gitextension(options: GitExtensionOpts) extends LayerCreator {
   override val overlayName: String = Gitextension.overlayName
   override val description: String = Gitextension.description
 
   /**
-   * This method is executed when the user issues the command
-   * `run.myextension`.
-   * */
+    * This method is executed when the user issues the command
+    * `run.myextension`.
+    * */
   override def create(context: LayerCreatorContext,
                       serializeInverse: Boolean): Unit = {
     val cpg = context.cpg
     val serializedCpg = initSerializedCpg(context.outputDir, "gitextension", 0)
-    new DetermineChangedFilesPass(cpg).createApplySerializeAndStore(serializedCpg, serializeInverse)
+    new DetermineChangedFilesPass(cpg)
+      .createApplySerializeAndStore(serializedCpg, serializeInverse)
     serializedCpg.close()
   }
 
-  private class DetermineChangedFilesPass(cpg : Cpg) extends CpgPass(cpg) {
+  private class DetermineChangedFilesPass(cpg: Cpg) extends CpgPass(cpg) {
     override def run(): Iterator[DiffGraph] = {
       implicit val diffGraph: DiffGraph.Builder = DiffGraph.newBuilder
       val changedFiles = recentlyChangedFiles().map(_.toAbsolutePath.toString)
-      cpg.file.name(changedFiles :_* ).newTagNode("RECENTLY_CHANGED").store
+      cpg.file.name(changedFiles: _*).newTagNode("RECENTLY_CHANGED").store
       Iterator(diffGraph.build)
     }
 
-    private def recentlyChangedFiles(commitsToGoBack : Int = 10): List[Path] = {
+    private def recentlyChangedFiles(commitsToGoBack: Int = 10): List[Path] = {
       val builder = new FileRepositoryBuilder()
-      val repository = builder.setGitDir(File(options.pathToRepo).toJava).setMustExist(true)
+      val repository = builder
+        .setGitDir(File(options.pathToRepo).toJava)
+        .setMustExist(true)
         .build()
       val git = new Git(repository)
       val reader = repository.newObjectReader()
 
-      def createIterFor(commit : String) = {
+      def createIterFor(commit: String) = {
         val iter = new CanonicalTreeParser()
         iter.reset(reader, repository.resolve(commit))
         iter
       }
 
-      val nCommitsAvailable = git.log().setMaxCount(commitsToGoBack).call().asScala.toList.size
-      if(nCommitsAvailable == 0) {
+      val nCommitsAvailable =
+        git.log().setMaxCount(commitsToGoBack).call().asScala.toList.size
+      if (nCommitsAvailable == 0) {
         List()
       } else {
-        val oldTreeIter = createIterFor(s"HEAD~${nCommitsAvailable-1}^{tree}")
+        val oldTreeIter = createIterFor(s"HEAD~${nCommitsAvailable - 1}^{tree}")
         val newTreeIter = createIterFor(s"HEAD^{tree}")
         val diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE)
         diffFormatter.setRepository(repository)
-        diffFormatter.scan(oldTreeIter, newTreeIter).asScala.toList.map{ entry =>
-          Paths.get(repository.getDirectory.getParent, entry.getNewPath)
+        diffFormatter.scan(oldTreeIter, newTreeIter).asScala.toList.map {
+          entry =>
+            Paths.get(repository.getDirectory.getParent, entry.getNewPath)
         }
       }
     }
@@ -104,9 +109,9 @@ class Gitextension(options : GitExtensionOpts) extends LayerCreator {
   }
 
   /**
-   * This method may be implemented to check whether the extension has
-   * already been run by inspecting the graph. This method is only
-   * relevant for legacy CPGs. You can set it to false on new extensions.
-   * */
+    * This method may be implemented to check whether the extension has
+    * already been run by inspecting the graph. This method is only
+    * relevant for legacy CPGs. You can set it to false on new extensions.
+    * */
   override def probe(cpg: Cpg): Boolean = false
 }
